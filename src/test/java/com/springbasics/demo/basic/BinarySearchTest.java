b0ad3ff7c78ed1c76cec15f8d9b6d512d518095e
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-11-30 13:50:26 -0700

    adds binary searh test

---
+++ src/test/java/com/springbasics/demo/basic/BinarySearchTest.java
@@ -0,0 +1,32 @@
+package com.springbasics.demo.basic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import com.springbasics.demo.DemoApplicationBasic;
+
+//Load the context 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = DemoApplicationBasic.class)
+public class BinarySearchTest {
+
+	//get this bean from the context
+	@Autowired
+	BinarySearchImpl binarySearch;
+	
+	@Test
+	public void testBasicScenario() {
+		int result = binarySearch.binarySearch(new int[] {}, 5);
+		//call method on binarySearch
+		//check if the value is correct
+		
+		assertEquals(3, result);
+	}
+
+}
